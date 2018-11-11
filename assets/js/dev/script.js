63a8fe59d9aa1528691d487eaeb118c162fbe75c
Brent Robbins <brent@variantstudios.com>
2018-11-11 16:25:25 -0700

    Hero setup.

--- assets/js/dev/script.js
+++ assets/js/dev/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+function ready() {
   console.log('locked and loaded 🚀');
 
   // Add a class of .mq if the browser supports Media Query and '.no-mq' if it doesn't
@@ -23,19 +23,4 @@ $(document).ready(function() {
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
   });
-
-  // Cloudinary:
-  // function Cloudinary() {
-  //   for (var t = 0; t < document.images.length; t++)
-  //     (!(e = document.images[t]).complete || (void 0 !== e.naturalWidth && 0 == e.naturalWidth)) &&
-  // 			(console.log(document.images[t]),
-  // 			document.images[t].attributes['data-img'] &&
-  // 				(document.images[t].src = document.images[t].attributes['data-img'].value),
-  // 			document.images[t].attributes['data-width'] &&
-  // 				(document.images[t].width = document.images[t].attributes['data-width'].value),
-  // 			document.images[t].attributes['data-height'] &&
-  // 				(document.images[t].height = document.images[t].attributes['data-height'].value));
-  //   var e;
-  // }
-  // Cloudinary();
-});
+}
