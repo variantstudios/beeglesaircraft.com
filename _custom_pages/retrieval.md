71346340b32981a42581a6fd93f1c83ad320bb1e
Brent Robbins <brent@variantstudios.com>
2018-11-25 23:40:37 -0700

    page gallery setup

--- _custom_pages/retrieval.md
+++ _custom_pages/retrieval.md
@@ -6,9 +6,14 @@ search_engine_optimization:
     Beegles Aircraft Service Inc, provides safe and secure transportation of
     aircraft.
 hero_image: /assets/images/hero/final3.jpg
+page_image_gallery:
+  - image_title: "Add a photo title here"
+    gallery_image_path: "/assets/images/home.png"
+  - image_title: "Add a photo title here"
+    gallery_image_path: "/assets/images/home.png"
+  - image_title: "Add a photo title here"
+    gallery_image_path: "/assets/images/home.png"
 layout: page
 ---
-
-## Aircraft Retrieval
 
 Beegles Aircraft Service Inc, provides safe and secure transportation of aircraft. Our specialized team will make sure your aircraft gets to our facility without any further damage utilizing our many specialized fixtures to protect your aircraft. We have traveled from coast to coast and border to border dismantling, rigging, and securing the aircraft for transportation. We also respond very quickly to unfortunate accidents standing by to assist FAA, NTSB, aircraft representatives, and emergency personnel. Beegles Aircraft has gone high and low doing these recoveries, often using helicopters to retrieve high mountain accidents and divers to recover the underwater accidents. Should you ever need our specialized service, we have the experienced personal and equipment to successfully retrieve aircraft and transport to our facility for storage or repair.
--- assets/js/dev/script.js
+++ assets/js/dev/script.js
@@ -54,24 +54,26 @@ $(document).ready(function() {
     // we shouldn't be responsive
     breakpoints: [ [ 1199, 4 ], [ 979, 3 ], [ 768, 2 ], [ 479, 1 ] ]
   };
+  var options2 = {
+    draggingMS: 300,
+    controlsMS: 800,
+    autoplayMS: 5000,
+    returnToStart: true,
+    returnToStartMS: 1000,
+    stopOnHover: true,
+    navigation: [ '&larr;', '&rarr;' ],
+    pagination: true,
+    paginationNumbers: false,
+    items: 1
+  };
   if ($('.carousel-revolver').length) {
     $('.carousel-revolver').revolver(options);
+  }
+  if ($('.carousel-revolver-gallery').length) {
+    $('.carousel-revolver-gallery').revolver(options2);
   }
   // lightbox
   if ($('.image-link').length) {
     $('.image-link').magnificPopup({ type: 'image' });
   }
-});
-//cloudinary
-$(document).ready(function() {
-  for (var t = 0; t < document.images.length; t++)
-    (!(e = document.images[t]).complete || (void 0 !== e.naturalWidth && 0 == e.naturalWidth)) &&
-			(console.log(document.images[t]),
-			document.images[t].attributes['data-img'] &&
-				(document.images[t].src = document.images[t].attributes['data-img'].value),
-			document.images[t].attributes['data-width'] &&
-				(document.images[t].width = document.images[t].attributes['data-width'].value),
-			document.images[t].attributes['data-height'] &&
-				(document.images[t].height = document.images[t].attributes['data-height'].value));
-  var e;
 });
