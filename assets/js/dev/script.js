a8488a44d961bc2235f7b411f859456c3b4ed038
Brent Robbins <brent@variantstudios.com>
2018-11-25 21:47:18 -0700

    Initial carousel, gallery, lightbox setup.

--- assets/js/dev/script.js
+++ assets/js/dev/script.js
@@ -1,12 +1,12 @@
-function ready() {
+$(document).ready(function() {
   console.log('locked and loaded 🚀');
 
   // Add a class of .mq if the browser supports Media Query and '.no-mq' if it doesn't
-  if (Modernizr.mq('only all')) {
-    $('html').addClass('mq');
-  } else {
-    $('html').addClass('no-mq');
-  }
+  // if (Modernizr.mq('only all')) {
+  //   $('html').addClass('mq');
+  // } else {
+  //   $('html').addClass('no-mq');
+  // }
 
   // Listen to a test, give it a callback
   Modernizr.on('flexbox', function(result) {
@@ -23,4 +23,42 @@ function ready() {
     $('.navbar-burger').toggleClass('is-active');
     $('.navbar-menu').toggleClass('is-active');
   });
-}
+  // Carousel revolver
+  var options = {
+    // The transition speed if the user drags the panels
+    draggingMS: 300,
+
+    // The transition speed if the user selects a pagination item
+    controlsMS: 800,
+
+    // False if no autoplay, or a duration in milliseconds
+    autoplayMS: 5000,
+
+    returnToStart: true,
+    returnToStartMS: 1000,
+
+    // Whether or not hovering will cause the autoplayMS to pause
+    stopOnHover: true,
+
+    // Should we have previous and next buttons, and what should they say?
+    navigation: [ '&larr;', '&rarr;' ],
+
+    // Should we have pagination, and should it include numbers in the html?
+    pagination: true,
+    paginationNumbers: false,
+
+    // The number of items to show within the revolver
+    items: 3,
+
+    // An array of [pixel width, visible items] or false if
+    // we shouldn't be responsive
+    breakpoints: [ [ 1199, 4 ], [ 979, 3 ], [ 768, 2 ], [ 479, 1 ] ]
+  };
+  if ($('.carousel-revolver').length) {
+    $('.carousel-revolver').revolver(options);
+  }
+  // lightbox
+  if ($('.image-link').length) {
+    $('.image-link').magnificPopup({ type: 'image' });
+  }
+});
